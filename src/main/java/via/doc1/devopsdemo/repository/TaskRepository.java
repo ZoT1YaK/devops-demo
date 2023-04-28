078374f8f448a124e66b6dd0a111740c4e7cc7e2
ZoT1YaK <robert.zustars@gmail.com>
2023-04-28 08:11:47 +0000

    u

--- src/main/java/via/doc1/devopsdemo/repository/TaskRepository.java
+++ src/main/java/via/doc1/devopsdemo/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package via.doc1.devopsdemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import via.doc1.devopsdemo.model.Task;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, String>{
     
 }
--- src/main/java/via/doc1/devopsdemo/repository/TeamMemberRepository.java
+++ src/main/java/via/doc1/devopsdemo/repository/TeamMemberRepository.java
@@ -2,7 +2,9 @@ package via.doc1.devopsdemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import via.doc1.devopsdemo.model.TeamMember;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeamMemberRepository extends JpaRepository<TeamMember, String> { 
 
 }
